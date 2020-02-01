6462ae40c4fa15324416e0897fd12470029f5a6e
Paul Tagliamonte <paultag@gmail.com>
2020-02-01 18:27:17 -0500

    Look up the KMS String to use to Signature based on pubkey / opts (#1)
    * Look up the KMS String to use to Signature based on pubkey / opts
    
    * Output an error for RSA too

--- kmssigner/signer.go
+++ kmssigner/signer.go
@@ -2,9 +2,13 @@ package kmssigner
 
 import (
 	"context"
-	"crypto"
-	"crypto/x509"
+	"fmt"
 	"io"
+
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"crypto/x509"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kms"
@@ -40,11 +44,16 @@ func (k kmsSigner) Public() crypto.PublicKey {
 }
 
 func (k kmsSigner) Sign(_ io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	signingAlgorithm, err := kmsSigningAlgorithm(k.pubKey, opts)
+	if err != nil {
+		return nil, err
+	}
+
 	signatureResponse, err := k.kmsapi.SignWithContext(context.TODO(), &kms.SignInput{
 		KeyId:            aws.String(k.keyArn),
 		Message:          digest,
 		MessageType:      aws.String("DIGEST"),
-		SigningAlgorithm: aws.String(cryptoHashToKmsHash[opts.HashFunc()]),
+		SigningAlgorithm: aws.String(signingAlgorithm),
 	})
 	if err != nil {
 		return nil, err
@@ -52,7 +61,42 @@ func (k kmsSigner) Sign(_ io.Reader, digest []byte, opts crypto.SignerOpts) ([]b
 	return signatureResponse.Signature, nil
 }
 
-// TODO: Fill in more hashes and take into account key type.
-var cryptoHashToKmsHash = map[crypto.Hash]string{
-	crypto.SHA256: kms.SigningAlgorithmSpecEcdsaSha256,
+// Given a public key and the signature options, return the right string to
+// send to KMS so that we get a valid signature out.
+func kmsSigningAlgorithm(pub crypto.PublicKey, opts crypto.SignerOpts) (string, error) {
+	switch pub.(type) {
+	case *ecdsa.PublicKey:
+		switch opts.HashFunc() {
+		case crypto.SHA256:
+			return kms.SigningAlgorithmSpecEcdsaSha256, nil
+		case crypto.SHA384:
+			return kms.SigningAlgorithmSpecEcdsaSha384, nil
+		case crypto.SHA512:
+			return kms.SigningAlgorithmSpecEcdsaSha512, nil
+		default:
+			return "", fmt.Errorf("hallow/kmssigner: unknown hash algorithm for use with ECDSA")
+		}
+	case *rsa.PublicKey:
+		// We currently only support PKCS1v1.5 because we're we're bad people
+		// and feel bad about it.
+		//
+		// We may add a config bool (UsePSSPadding or something) to flip this
+		// to PSS in the future, but for now, since most things expect
+		// PKCS1v1.5, and people will be mad at us if we just, well, don't
+		// support RSA at all.
+		//
+		// So, here we are.
+		switch opts.HashFunc() {
+		case crypto.SHA256:
+			return kms.SigningAlgorithmSpecRsassaPkcs1V15Sha256, nil
+		case crypto.SHA384:
+			return kms.SigningAlgorithmSpecRsassaPkcs1V15Sha384, nil
+		case crypto.SHA512:
+			return kms.SigningAlgorithmSpecRsassaPkcs1V15Sha512, nil
+		default:
+			return "", fmt.Errorf("hallow/kmssigner: unknown hash algorithm for use with RSA")
+		}
+	}
+
+	return "", fmt.Errorf("hallow/kmsigner: unknown key algorithm")
 }
