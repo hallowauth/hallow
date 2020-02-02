83a98dc543229702aabcb58da6cae997f82cec85
Alex Gaynor <alex.gaynor@gmail.com>
2020-02-01 19:43:13 -0500

    TF module for creating all the needed resources

---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "request_certificate_policy_arn" {
+  value = aws_iam_policy.request_certificate_policy.arn
+}
+
+output "hallow_endpoint" {
+  value = "${aws_api_gateway_deployment.prod.invoke_url}${aws_api_gateway_resource.hallow_resource.path}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "lambda_s3_bucket" {
+  type = string
+}
+
+variable "lambda_s3_key" {
+  type = string
+}
