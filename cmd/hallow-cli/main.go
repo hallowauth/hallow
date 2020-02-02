0e3f5b13ed04b07ad8b7bd7f962bea253a306ec1
Paul Tagliamonte <paultag@gmail.com>
2020-02-02 01:29:55 -0500

    This adds a sign subcommand to sign public keys (#17)
    * Add a sign subcommand to sign all passed pubkeys
    
    * clean code up

--- cmd/hallow-cli/main.go
+++ cmd/hallow-cli/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
+	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/urfave/cli/v2"
+
+	"github.com/alex/hallow/client"
 )
+
+//
+func hallowClientFromCLI(c *cli.Context) client.Client {
+	return client.New(session.New(), http.DefaultClient, c.String("endpoint"))
+}
 
 func main() {
 	app := &cli.App{
 		Name:  "hallow-cli",
 		Usage: "talk to the hallow server",
+		Before: func(c *cli.Context) error {
+			level, err := log.ParseLevel(c.String("log-level"))
+			if err != nil {
+				return err
+			}
+			log.SetLevel(level)
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "log-level",
+				EnvVars: []string{"HALLOW_LOG_LEVEL"},
+				Value:   "info",
+			},
+			&cli.StringFlag{
+				Name:    "endpoint",
+				EnvVars: []string{"HALLOW_ENDPOINT"},
+				Value:   "",
+			},
+		},
 		Commands: []*cli.Command{
 			SignCommand,
 			GetPubKeyCommand,
