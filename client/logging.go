f9a59fdce29b3750b1eb5a99d255c2b4bdb2d028
Paul R. Tagliamonte <paultag@gmail.com>
2020-02-02 21:28:36 -0500

    Fix logging request => hallow

--- client/logging.go
+++ client/logging.go
@@ -9,11 +9,11 @@ import (
 // Create a new log.Entry from an ssh.Certificate
 func logWithCertificate(sshCert *ssh.Certificate) *log.Entry {
 	return log.WithFields(log.Fields{
-		"response.certificate.type":         sshCert.Type(),
-		"response.certificate.serial":       sshCert.Serial,
-		"response.certificate.key_id":       sshCert.KeyId,
-		"response.certificate.valid_after":  sshCert.ValidAfter,
-		"response.certificate.valid_before": sshCert.ValidBefore,
-		"response.certificate.principals":   sshCert.ValidPrincipals,
+		"hallow.certificate.type":         sshCert.Type(),
+		"hallow.certificate.serial":       sshCert.Serial,
+		"hallow.certificate.key_id":       sshCert.KeyId,
+		"hallow.certificate.valid_after":  sshCert.ValidAfter,
+		"hallow.certificate.valid_before": sshCert.ValidBefore,
+		"hallow.certificate.principals":   sshCert.ValidPrincipals,
 	})
 }
